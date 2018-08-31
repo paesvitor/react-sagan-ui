cb27f52adbe998c9a23ae9c872a22b7c27a0548e
Vitor Paes <vitor.paes@farfetch.com>
2018-08-31 18:24:09 -0300

    feat: Added gray color

--- src/config/colors/index.js
+++ src/config/colors/index.js
@@ -4,17 +4,19 @@ import green from './green'
 import purple from './purple'
 import blue from './blue'
 import yellow from './yellow'
+import gray from './gray'
 
 const white = '#fff'
 const black = '#000'
 
 export default {
-    red,
-    purple,
-    pink,
-    green,
-    white,
-    black,
-    blue,
-    yellow
+  red,
+  purple,
+  pink,
+  green,
+  white,
+  black,
+  blue,
+  yellow,
+  gray
 }
