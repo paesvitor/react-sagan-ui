a1f883cfa82fbfc483f8736f35efdbdafaac4296
Vitor Paes <vitor.paes@farfetch.com>
2018-09-05 17:54:42 -0300

    chore: Updated docz description

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Sagan UI',
-    description: 'This is my awesome documentation',
+    description: 'React UI component library',
     themeConfig: {
       colors: {
         primary: '#3F51B5',
