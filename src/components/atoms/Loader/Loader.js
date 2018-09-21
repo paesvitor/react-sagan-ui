08e427c8a8ce95bb2f6d4e3c4db883a10c0ef23f
Vitor Paes <vitor.paes@farfetch.com>
2018-09-20 22:11:51 -0300

    feat: Add initial Loader

---
+++ src/components/atoms/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import ElipsisLoader from "./types/ElipsisLoader";
+
+const StyledLoader = styled.div``;
+
+const Loader = props => {
+  return <ElipsisLoader />;
+};
+
+export default Loader;
